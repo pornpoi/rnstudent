35c02263be36077be51b1d7cff3e1bd376e04e53
codemobiles <chaiyasit.t@gmail.com>
2020-10-26 14:12:37 +0700

    1234

--- rnstudent/demo2/src/RegisterScreen.js
+++ rnstudent/demo2/src/RegisterScreen.js
@@ -6,11 +6,15 @@ export default function RegisterScreen() {
     <ImageBackground
       style={{flex: 1}}
       source={require('./assets/img/gradient_bg.png')}>
+      {/* Banner */}
       <Image
         style={{height: 120, width: '100%'}}
         resizeMode="contain"
         source={require('./assets/img/header_react_native.png')}
       />
+
+      {/* Authen section */}
+      <View style={{height: 100, backgroundColor: 'white'}}></View>
     </ImageBackground>
   );
 }
