a59d823b0772f4b9ee1963e5e8377daa4f92cfcf
codemobiles <chaiyasit.t@gmail.com>
2020-10-26 11:08:16 +0700

    1234

--- rnstudent/demo1/App.js
+++ rnstudent/demo1/App.js
@@ -1,18 +1,17 @@
 import React from 'react';
 import {View, Text} from 'react-native';
 
-export default function App() {  
+export default function App() {
   return (
     <View
-    // jsx
+      // jsx
       style={{
-        flex:0.5,
+        flex: 0.5,
         backgroundColor: 'red',
         flexDirection: 'column',
         justifyContent: 'center',
         alignItems: 'center',
       }}>
-        
       <Text>Hey</Text>
     </View>
   );
