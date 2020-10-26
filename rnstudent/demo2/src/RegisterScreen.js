1ade2221b552132cdbfc83fb9438cc829c920dff
codemobiles <chaiyasit.t@gmail.com>
2020-10-26 13:54:43 +0700

    1234

--- rnstudent/demo2/src/RegisterScreen.js
+++ rnstudent/demo2/src/RegisterScreen.js
@@ -3,7 +3,7 @@ import {View, Text} from 'react-native';
 
 export default function RegisterScreen() {
   return (
-    <View>
+    <View style={{flex: 1, backgroundColor: 'red'}}>
       <Text>RegisterScreen</Text>
     </View>
   );
