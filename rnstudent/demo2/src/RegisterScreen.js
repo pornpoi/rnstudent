fbfb5e3264b3a340b4996a3c210d50db7074320a
codemobiles <chaiyasit.t@gmail.com>
2020-10-26 14:08:56 +0700

    1234

--- rnstudent/demo2/src/RegisterScreen.js
+++ rnstudent/demo2/src/RegisterScreen.js
@@ -1,11 +1,16 @@
 import React from 'react';
-import {View, Text, ImageBackground} from 'react-native';
+import {View, Text, ImageBackground, Image} from 'react-native';
 
 export default function RegisterScreen() {
   return (
     <ImageBackground
       style={{flex: 1}}
       source={require('./assets/img/gradient_bg.png')}>
+      <Image
+        style={{height: 80, width: '100%'}}
+        resizeMode="contain"
+        source={require('./assets/img/header_react_native.png')}
+      />
       <Text>RegisterScreen</Text>
     </ImageBackground>
   );
