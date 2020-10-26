87601bcbd521bdf978413c261325519cdb9fcbd1
codemobiles <chaiyasit.t@gmail.com>
2020-10-26 12:01:24 +0700

    1234

--- rnstudent/demo1/App2.js
+++ rnstudent/demo1/App2.js
@@ -1,16 +1,60 @@
 import React from 'react';
-import {View, Text} from 'react-native';
+import {View, Text, Dimensions} from 'react-native';
 
 export default function App2() {
   return (
     <View
+      // jsx
       style={{
         flex: 1,
-        backgroundColor: 'yellow',
-        flexDirection: 'column',
-        justifyContent: 'flex-end',
+        backgroundColor: 'white',
+        flexDirection: 'row',
+        justifyContent: 'space-evenly',
       }}>
-      <Text>1234</Text>
+      {/* Box1 */}
+      <Text
+        style={{
+          textAlignVertical: 'center',
+          color: 'black',
+          backgroundColor: 'yellow',
+          textAlign: 'center',
+        }}>
+        Hey 1
+      </Text>
+
+      {/* Box2 */}
+      <Text
+        style={{
+          textAlignVertical: 'center',
+          color: 'black',
+          backgroundColor: 'green',
+          textAlign: 'center',
+        }}>
+        Hey 2
+      </Text>
+
+      {/* Box3 */}
+      <Text
+        style={{
+          textAlignVertical: 'center',
+          color: 'black',
+          backgroundColor: '#3377ee30',
+          textAlign: 'center',
+        }}>
+        Hey 3
+      </Text>
+
+      <View
+        style={{
+          position: 'absolute',
+          top: Dimensions.get('screen').height / 2 - 50,
+          left: Dimensions.get('screen').width / 2 - 50,
+          height: 100,
+          width: 100,
+          borderRadius: 50,
+          backgroundColor: 'black',
+        }}
+      />
     </View>
   );
 }
