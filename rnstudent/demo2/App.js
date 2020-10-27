316dcd83944ae8bcac45ff7fb502542211a3d445
codemobiles <chaiyasit.t@gmail.com>
2020-10-27 11:18:28 +0700

    1234

--- rnstudent/demo2/App.js
+++ rnstudent/demo2/App.js
@@ -1,8 +1,20 @@
 import React from 'react';
-import {View, Text} from 'react-native';
+import {View, SafeAreaView, StatusBar} from 'react-native';
+
 import HomeScreen from './src/HomeScreen';
 import RegisterScreen from './src/RegisterScreen';
 
+import AppNavigator from './src/AppNavigator';
+import {NavigationContainer} from '@react-navigation/native';
+
 export default function App() {
-  return <HomeScreen />;
+  return (
+    <NavigationContainer>
+      <View style={{flex: 1}}>
+        <StatusBar barStyle="dark-content" />
+        <SafeAreaView />
+        <AppNavigator />
+      </View>
+    </NavigationContainer>
+  );
 }
--- rnstudent/demo2/src/AppNavigator.js
+++ rnstudent/demo2/src/AppNavigator.js
@@ -1,10 +1,26 @@
-import React from 'react'
-import { View, Text } from 'react-native'
+import React from 'react';
+import {View, Text} from 'react-native';
 
-export default function AppNavigator() {
+import {createStackNavigator} from '@react-navigation/stack';
+import {createBottomTabNavigator} from '@react-navigation/bottom-tabs';
+
+import HomeScreen from './HomeScreen';
+import RegisterScreen from './RegisterScreen';
+import JSONFeedScreen from './JSONFeedScreen';
+import CameraScreen from './CameraScreen';
+import YoutubeScreen from './YoutubeScreen';
+
+const Stack = createStackNavigator();
+
+const RootStack = (props) => {
   return (
-    <View>
-      <Text></Text>
-    </View>
-  )
-}
+    <Stack.Navigator initialRouteName="Register">
+      <>
+        <Stack.Screen name="Home" component={HomeScreen} />
+        <Stack.Screen name="Register" component={RegisterScreen} />
+      </>
+    </Stack.Navigator>
+  );
+};
+
+export default RootStack;
