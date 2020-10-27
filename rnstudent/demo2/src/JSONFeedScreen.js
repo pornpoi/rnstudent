369843eb791767629bee9223078f6b3f8a46ab65
codemobiles <chaiyasit.t@gmail.com>
2020-10-27 14:45:59 +0700

    1234

--- rnstudent/demo2/src/JSONFeedScreen.js
+++ rnstudent/demo2/src/JSONFeedScreen.js
@@ -11,8 +11,56 @@ import {
 
 export default function JSONFeedScreen() {
   return (
-    <ImageBackground source={require('./assets/img/bg.png')}>
+    <ImageBackground
+      style={styles.container}
+      source={require('./assets/img/bg.png')}>
       <Text>JSONScreen</Text>
     </ImageBackground>
   );
 }
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+  },
+  list_header: {
+    width: '100%',
+    height: 100,
+  },
+  listCard: {
+    overflow: 'hidden',
+    flexDirection: 'column',
+    marginTop: 20,
+    marginLeft: 20,
+    marginRight: 20,
+    backgroundColor: '#FFF',
+    borderRadius: 20,
+    padding: 0,
+  },
+  listCardView: {
+    flexDirection: 'row',
+    marginBottom: 16,
+    height: 45,
+    alignItems: 'center',
+  },
+  listAvatar: {
+    width: 45,
+    height: '100%',
+    marginRight: 16,
+  },
+  listTitleSubtitleContainer: {
+    flexDirection: 'column',
+    marginRight: 16,
+    flex: 1,
+  },
+  listTitle: {
+    fontWeight: '700',
+  },
+  listSubTitle: {
+    fontWeight: '100',
+  },
+  listYoutbeImage: {
+    width: '100%',
+    height: 190,
+  },
+});
