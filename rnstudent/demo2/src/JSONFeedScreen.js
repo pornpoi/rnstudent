4615d9b106d851df74acde61699af935c080428e
codemobiles <chaiyasit.t@gmail.com>
2020-10-27 15:46:44 +0700

    1234

--- rnstudent/demo2/src/JSONFeedScreen.js
+++ rnstudent/demo2/src/JSONFeedScreen.js
@@ -53,11 +53,20 @@ export default function JSONFeedScreen() {
     </TouchableOpacity>
   );
 
+  const renderHeader = () => (
+    <Image
+      source={require('./assets/img/header_react_native.png')}
+      resizeMode="contain"
+      style={{height: 100, width: '100%'}}
+    />
+  );
+
   return (
     <ImageBackground
       style={styles.container}
       source={require('./assets/img/bg.png')}>
       <FlatList
+        ListHeaderComponent={renderHeader}
         data={dataArray ? dataArray : []}
         renderItem={renderRow}
         keyExtractor={(item) => item.id}
