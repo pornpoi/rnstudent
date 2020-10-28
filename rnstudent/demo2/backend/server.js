a4b1cc77e22f9354eb97f53a3180bf96089660e5
codemobiles <chaiyasit.t@gmail.com>
2020-10-28 13:18:11 +0700

    1234

--- rnstudent/demo2/backend/server.js
+++ rnstudent/demo2/backend/server.js
@@ -1,2 +1,14 @@
+const express = require('express');
+const app = express();
 
-console.log('777');
+app.get('/', (req, res) => {
+  res.end('1234');
+});
+
+app.get('/login/:username/:password', (req, res) => {
+  res.json(req.params);
+});
+
+app.listen(3000, () => {
+  console.log('Server running');
+});
