b44bb0da923ce50f02107dacb34b5b2040e95635
codemobiles <chaiyasit.t@gmail.com>
2020-10-28 15:02:28 +0700

    1234

--- rnstudent/demo2/App.js
+++ rnstudent/demo2/App.js
@@ -1,19 +1,29 @@
-import React from 'react';
+import React, {useState, useEffect} from 'react';
 import {View, SafeAreaView, StatusBar} from 'react-native';
 
 import HomeScreen from './src/HomeScreen';
 import RegisterScreen from './src/RegisterScreen';
 
-import AppNavigator from './src/AppNavigator';
+import AsyncStorage from '@react-native-community/async-storage';
 import {NavigationContainer} from '@react-navigation/native';
+import AppNavigator from './src/AppNavigator';
 
 export default function App() {
+  const [showLogin, setShowLogin] = useState(false);
+  const [isReady, setIsReady] = useState(false);
+  useEffect(() => {
+    AsyncStorage.getItem('token').then((token) => {
+      setShowLogin(token == null ? true : false);
+      setIsReady(true);
+    });
+  }, [isReady]);
+
   return (
     <NavigationContainer>
       <View style={{flex: 1}}>
         <StatusBar barStyle="dark-content" />
         <SafeAreaView />
-        <AppNavigator />
+        <AppNavigator showAuthen={showLogin} />
       </View>
     </NavigationContainer>
   );
