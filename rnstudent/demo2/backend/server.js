d5e7796e1c8c4cd1c848bb53cb951632317301b5
codemobiles <chaiyasit.t@gmail.com>
2020-10-28 13:27:08 +0700

    1234

--- rnstudent/demo2/backend/server.js
+++ rnstudent/demo2/backend/server.js
@@ -1,5 +1,7 @@
 const express = require('express');
 const app = express();
+const formidable = require('formidable');
+const fs = require('fs-extra');
 
 app.get('/', (req, res) => {
   res.end('1234');
@@ -7,6 +9,13 @@ app.get('/', (req, res) => {
 
 app.get('/login/:username/:password', (req, res) => {
   res.json(req.params);
+});
+
+app.post('/uploads', (req, res) => {
+  let form = new formidable.IncomingForm();
+  form.parse(req, (error, fields, files) => {
+    res.json({error, fields, files});
+  });
 });
 
 app.listen(3000, () => {
