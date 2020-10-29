217b01d503b946946dde27aacb4bc3cbce17d776
codemobiles <chaiyasit.t@gmail.com>
2020-10-29 09:58:55 +0700

    1234

--- rnstudent/demo3/src/ScannerScreen.js
+++ rnstudent/demo3/src/ScannerScreen.js
@@ -1,10 +1,80 @@
-import React from 'react'
-import { View, Text } from 'react-native'
+import React, {useState, useEffect, useRef} from 'react';
 
-export default function ScannerScreen() {
+import {View, StyleSheet, Text, TouchableOpacity} from 'react-native';
+
+import QRCodeScanner from 'react-native-qrcode-scanner';
+export default function ScannerScreen(props) {
+  const [isReady, setIsReady] = useState(false);
+  const scannerRef = useRef(null)
+
+  const showScanner = () => {
+    return (
+      <QRCodeScanner
+        ref={scannerRef}
+        showMarker
+        onRead={(e) => onSuccess(e.data)}
+        style={{flex: 1}}
+        bottomContent={
+          <TouchableOpacity
+            onPress={() => scannerRef.current.reactivate()}
+            style={styles.buttonTouchable}>
+            <Text style={styles.buttonText}>SCAN</Text>
+          </TouchableOpacity>
+        }
+      />
+    );
+  };
+
+  const showLoading = () => {
+    return (
+      <Text
+        style={{
+          flex: 1,
+          textAlign: 'center',
+          textAlignVertical: 'center',
+          color: 'white',
+        }}>
+        Loading...
+      </Text>
+    );
+  };
+
+  const onSuccess = result => {
+    const {onResult} = props.route.params;
+    onResult(result);
+    props.navigation.goBack();
+  };
+
   return (
-    <View>
-      <Text>ScannerScreen</Text>
+    <View style={{flex: 1, backgroundColor: 'black'}}>
+      {true ? showScanner() : showLoading()}
     </View>
-  )
+  );
 }
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    flexDirection: 'column',
+    justifyContent: 'flex-start',
+  },
+  centerText: {
+    flex: 1,
+    fontSize: 18,
+    padding: 32,
+    color: '#777',
+  },
+  buttonText: {
+    fontSize: 21,
+    fontWeight: 'bold',
+    color: '#FFF',
+  },
+  buttonTouchable: {
+    height: 50,
+    width: '100%',
+    alignSelf: 'center',
+    justifyContent: 'center',
+    alignItems: 'center',
+    backgroundColor: '#fa4a4d',
+  },
+});
