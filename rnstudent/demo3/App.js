460c2345860cb342a0429443755f0e2318890ab6
codemobiles <chaiyasit.t@gmail.com>
2020-10-29 10:54:52 +0700

    1234

--- rnstudent/demo3/App.js
+++ rnstudent/demo3/App.js
@@ -1,11 +1,12 @@
 import React from 'react';
-import {View, SafeAreaView, StatusBar} from 'react-native';
+import {View, SafeAreaView, StatusBar, LogBox} from 'react-native';
 import {NavigationContainer} from '@react-navigation/native';
 import AppNavigator from "./src/AppNavigator";
 
 export default function App() {
 
-  console.disableYellowBox = true
+  LogBox.ignoreAllLogs(true)
+  //console.disableYellowBox = true
 
   return (
     <NavigationContainer>
