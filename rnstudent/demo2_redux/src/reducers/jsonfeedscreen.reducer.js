af8b4bf3557d10fa7f1cef78e7c7a81e8044fd4d
codemobiles <chaiyasit.t@gmail.com>
2020-10-29 15:21:18 +0700

    1234

--- rnstudent/demo2_redux/src/reducers/jsonfeedscreen.reducer.js
+++ rnstudent/demo2_redux/src/reducers/jsonfeedscreen.reducer.js
@@ -1,3 +1,5 @@
+import {JSON_FEED_FAILED, JSON_FEED_FETCHING, JSON_FEED_SUCCESS} from '../constants';
+
 const initialState = {
   dataArray: [],
   isFetching: false,
@@ -6,9 +8,12 @@ const initialState = {
 
 export default (state = initialState, {type, payload}) => {
   switch (type) {
-    case typeName:
-      return {...state, ...payload};
-
+    case JSON_FEED_FETCHING:
+      return {...state, dataArray: [], isFetching: true, isError: false};
+    case JSON_FEED_SUCCESS:
+      return {...state, dataArray: payload, isFetching: false, isError: false};
+    case JSON_FEED_FAILED:
+      return {...state, dataArray: [], isFetching: false, isError: true};
     default:
       return state;
   }
