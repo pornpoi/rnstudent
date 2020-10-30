c6b08370cad76784d9c730e1a9bf2483ea625c74
codemobiles <chaiyasit.t@gmail.com>
2020-10-30 10:02:47 +0700

    1234

--- rnstudent/demo4/src/TabMapScreen.js
+++ rnstudent/demo4/src/TabMapScreen.js
@@ -1,10 +1,66 @@
-import React from 'react'
-import { View, Text } from 'react-native'
+import React, {useState, useEffect} from 'react';
+import {
+  StyleSheet,
+  View,
+  Text,
+  Image,
+  Dimensions,
+  TouchableOpacity,
+  Platform,
+} from 'react-native';
+import MapView, {Marker} from 'react-native-maps';
+import CustomCallout from './CustomCallout';
+import openMap from 'react-native-open-maps';
+const {width, height} = Dimensions.get('window');
+import {submitLocation, queryLocations} from './LocationTrackingAPI'
 
-export default function TabMapScreen() {
+const ASPECT_RATIO = width / height;
+// 13.6970244,100.5130343 codemobiles office
+const LATITUDE = 13.7853726;
+const LONGITUDE = 100.5714823;
+const LATITUDE_DELTA = 0.0022;
+const LONGITUDE_DELTA = LATITUDE_DELTA * ASPECT_RATIO;
+
+const TabMapScreen = () => {
+  const [markers, setMarkers] = useState([]);
+  const [region, setRegion] = useState({
+    latitude: LATITUDE,
+    longitude: LONGITUDE,
+    latitudeDelta: LATITUDE_DELTA,
+    longitudeDelta: LONGITUDE_DELTA,
+  });
+
+ 
+
   return (
-    <View>
-      <Text>TabMapScreen</Text>
+    <View style={styles.container}>
+      <MapView
+        onPress={e => addMarker(e.nativeEvent.coordinate)}
+        style={styles.map}
+        mapType="satellite" // NORMAL, SATELLITE, HYBRID
+      />
     </View>
-  )
-}
+  );
+};
+
+export default TabMapScreen;
+
+const styles = StyleSheet.create({
+  container: {
+    ...StyleSheet.absoluteFillObject,
+    justifyContent: 'flex-end',
+    height: '100%',
+    width: '100%',
+  },
+  banner: {
+    height: 80,
+    width: '100%',
+  },
+  map: {
+    ...StyleSheet.absoluteFillObject,
+  },
+  customView: {
+    width: Platform.OS == 'ios' ? 190 : 160,
+    height: 100,
+  },
+});
